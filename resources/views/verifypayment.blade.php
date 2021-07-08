4104db6ad95fdb3d0c021b4d73e02a929b264ab9
erfanad1992 <erfandn1992@gmail.com>
2021-07-08 17:45:37 +0430

    11111

--- resources/views/verifypayment.blade.php
+++ resources/views/verifypayment.blade.php
@@ -1,7 +1,7 @@
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <div style="text-align: center">
     <h1>نتیجه پرداخت</h1>
 
